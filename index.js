127431ff049b375726f6f2a7021db7de22bd7a89
Pierce <pierceevans@me.com>
2022-12-22 10:40:45 -0600

    commit tree a54cf50bce02bb1c44a9b7d6372e4a593c542512

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(name, eventName) {
+    let messages = [];
+    for(let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown() {
+let num = 10;
+while(num > -1) {
+    console.log(num--);
+}
+}
